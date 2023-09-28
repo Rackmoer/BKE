e8f3116ec6b7b7231cd02ea97579e775a3b2ab35
JulianKuijf1 <9284013-JulianKuijf1@users.noreply.replit.com>
2023-09-28 16:28:32 +0000

    Added myagent and validation process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bke import MLAgent, is_winner, opponent
+from bke import MLAgent, is_winner, opponent, RandomAgent, train, validate, plot_validation
+import random
 
 class MyAgent(MLAgent):
   def evaluate(self, board):
@@ -9,3 +10,13 @@ class MyAgent(MLAgent):
     else:
       reward = 0
     return reward
+
+my_agent = MyAgent(1, 0.1)
+train(my_agent, 10000)
+
+random.seed(1)
+random_agent = RandomAgent()
+
+validation_agent = random_agent
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=1000)
+plot_validation(validation_result)
